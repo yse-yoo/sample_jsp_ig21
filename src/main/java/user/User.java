8c4bdd29d10282950ce506688fa94ef7513f5fb4
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-07-08 12:12:29 +0900

    add User servlet

---
+++ src/main/java/user/User.java
@@ -0,0 +1,20 @@
+package user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
+public class User extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//viewの表示
+		getServletContext().getRequestDispatcher("/user/index.jsp").forward(request, response);
+	}
+
+}
