1086ab471bdcbb6575973c646152503fb1ca6605
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-07-08 10:40:21 +0900

    add hello servlet

--- src/main/java/sample/Hello.java
+++ src/main/java/sample/Hello.java
@@ -8,16 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Hello")
+@WebServlet("/hello")
 public class Hello extends HttpServlet {
-	private static final long serialVersionUID = 1L;
        
-    public Hello() {
-        super();
-    }
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Hello, Servlet");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
